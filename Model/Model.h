9a3556b90f4cc54fd0c69fc01195cd3ca05bf6ab
watislaf <45079123+watislaf@users.noreply.github.com>
2020-03-18 19:44:54 +0300

    update Model

--- Model/Model.h
+++ Model/Model.h
@@ -7,23 +7,22 @@
 #include "Road.h"
 
 struct Map {
+
+};
+
+class Model {
+ public:
+  ~Model() = default;
+ private:
+  // Data that update all time
   std::vector<GameObject*> obj_handler;
   std::vector<std::vector<Wave>> Rounds;
   std::vector<Road*> roads;
   // vector<Building*> buildings;
   std::vector<Enemy*> enemies;
-};
-
-class Model {
- public:
-  // void SetDataBase(int level_id);
-  // Wave GetWave(int wave_id, int road_id);
-  // Wave SumWaves(Wave*, Wave*)
-  ~Model() = default;
-  Map map;
- private:
   int gold_{};
   int score_{};
+  // Data that update ones
 };
 
 #endif  // MODEL_MODEL_H_
