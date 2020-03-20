b90d371f6aae1a667606233b3eedb3e4cb5e275e
watislaf <45079123+watislaf@users.noreply.github.com>
2020-03-20 13:34:07 +0300

    update model.

--- Model/model.cpp
+++ Model/model.cpp
@@ -1,1 +1,27 @@
 #include "model.h"
+
+void Model::SetGameModel(int level_id) {
+/*  current_round_numbe = 0;
+  gold = 0;
+  map.score = 0;
+
+  switch (level_id) {
+    case 0:rounds_count = 1;
+      // to be changed, initialize  waves, only for 1 road and 1 round
+      time_to_prepair = 1000;
+      time_btwn_waves = 10000;
+      Rounds.resize(map.rounds_count);
+      Wave tmp_wave;
+      tmp_wave.frequency = 1000;
+      tmp_wave.road_num = 0;
+      tmp_wave.enemyid_times.emplace_back(1, 1);
+      Rounds[0].resize(map.roads_count);
+      Rounds[0][0] = tmp_wave;
+      // initialize roads
+      roads.resize(map.roads_count);
+      Road tmp_road;
+      tmp_road.direction_points = {{0, 0}, {100, 100}};
+      roads[0] = tmp_road;
+      //
+  }*/
+}
