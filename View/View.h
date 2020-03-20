0ce2947197791ef1216d0f1511172aa18bcf801f
Stanislav Katsuba <stas.katsuba@gmail.com>
2020-03-20 11:00:39 +0300

    Fixed issues with travis and smart pointers

--- View/View.h
+++ View/View.h
@@ -13,7 +13,7 @@ class View : public QMainWindow {
   ~View() = default;
 
  private:
-  std::shared_ptr<AbstractController> controller_;
+  AbstractController* controller_;
 };
 
 #endif  // VIEW_VIEW_H_
