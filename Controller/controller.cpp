7d2059a8f0c3fc7bfe5c1ea1aa9a4fd249a9e5db
elizabethfeden <elizabethfeden@gmail.com>
2020-03-27 12:03:58 +0300

    bugfix

--- Controller/controller.cpp
+++ Controller/controller.cpp
@@ -162,6 +162,10 @@ void Controller::MousePress(Coordinate position) {
     return;
   }
 
+  if(view_->GetTowerMenu() == nullptr) {
+    return;
+  }
+  
   // Check if tower menu element was pressed
   auto pressed = view_->GetTowerMenu()->GetPressedOption(position);
   if (pressed != nullptr) {
