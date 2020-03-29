ea2a539cc5afae151abc4686070acd183bcbf996
Stanislav Katsuba <stas.katsuba@gmail.com>
2020-03-29 20:41:57 +0300

    Removed unused conditions.

--- Model/model.cpp
+++ Model/model.cpp
@@ -85,65 +85,52 @@ void Model::LoadLevelFromJson(int level) {
   QJsonObject json_object =
       QJsonDocument::fromJson(level_file.readAll()).object();
 
-  time_between_ronds_ = 0;
-  if (json_object.contains("time_between_rounds")) {
-    time_between_ronds_ = json_object["time_between_rounds"].toInt();
-  }
-  gold_ = 0;
-  if (json_object.contains("gold")) {
-    gold_ = json_object["gold"].toInt();
-  }
-  score_ = 0;
-  if (json_object.contains("score")) {
-    score_ = json_object["score"].toInt();
-  }
+  time_between_ronds_ = json_object["time_between_rounds"].toInt();
+  gold_ = json_object["gold"].toInt();
+  score_ = json_object["score"].toInt();
 
   roads_.clear();
-  if (json_object.contains("roads")) {
-    QJsonArray json_roads = json_object["roads"].toArray();
-    roads_count_ = json_roads.size();
-    roads_.reserve(roads_count_);
+  QJsonArray json_roads = json_object["roads"].toArray();
+  roads_count_ = json_roads.size();
+  roads_.reserve(roads_count_);
 
-    QJsonArray json_road_nodes;
-    QJsonObject json_node;
-    for (int i = 0; i < roads_count_; i++) {
-      json_road_nodes = json_roads[i].toArray();
+  QJsonArray json_road_nodes;
+  QJsonObject json_node;
+  for (int i = 0; i < roads_count_; i++) {
+    json_road_nodes = json_roads[i].toArray();
 
-      int node_count = json_road_nodes.size();
-      std::vector<Coordinate> nodes;
-      nodes.reserve(node_count);
-      for (int j = 0; j < node_count; j++) {
-        json_node = json_road_nodes[j].toObject();
-        nodes.emplace_back(json_node["x"].toDouble(),
-                           json_node["y"].toDouble());
-      }
-      roads_.emplace_back(nodes);
+    int node_count = json_road_nodes.size();
+    std::vector<Coordinate> nodes;
+    nodes.reserve(node_count);
+    for (int j = 0; j < node_count; j++) {
+      json_node = json_road_nodes[j].toObject();
+      nodes.emplace_back(json_node["x"].toDouble(),
+                         json_node["y"].toDouble());
     }
+    roads_.emplace_back(nodes);
   }
 
   enemy_groups_.clear();
-  if (json_object.contains("rounds")) {
-    QJsonArray json_rounds = json_object["rounds"].toArray();
-    rounds_count_ = json_rounds.size();
-    enemy_groups_.reserve(rounds_count_);
+  QJsonArray json_rounds = json_object["rounds"].toArray();
+  rounds_count_ = json_rounds.size();
+  enemy_groups_.reserve(rounds_count_);
 
-    QJsonArray json_enemy_groups;
-    QJsonObject json_enemy_group;
-    for (int i = 0; i < rounds_count_; i++) {
-      json_enemy_groups = json_rounds[i].toArray();
+  QJsonArray json_enemy_groups;
+  QJsonObject json_enemy_group;
+  for (int i = 0; i < rounds_count_; i++) {
+    json_enemy_groups = json_rounds[i].toArray();
 
-      int group_count = json_enemy_groups.size();
-      std::vector<EnemyGroup> groups;
-      groups.reserve(group_count);
-      for (int j = 0; j < group_count; j++) {
-        json_enemy_group = json_enemy_groups[j].toObject();
-        groups.emplace_back(json_enemy_group["spawn_period"].toInt(),
-                            json_enemy_group["enemy_id"].toInt(),
-                            json_enemy_group["time_of_next_spawn"].toInt(),
-                            json_enemy_group["group_size"].toInt(),
-                            json_enemy_group["road_to_spawn"].toInt());
-      }
-      enemy_groups_.push_back(std::move(groups));
+    int group_count = json_enemy_groups.size();
+    std::vector<EnemyGroup> groups;
+    groups.reserve(group_count);
+    for (int j = 0; j < group_count; j++) {
+      json_enemy_group = json_enemy_groups[j].toObject();
+      groups.emplace_back(json_enemy_group["spawn_period"].toInt(),
+                          json_enemy_group["enemy_id"].toInt(),
+                          json_enemy_group["time_of_next_spawn"].toInt(),
+                          json_enemy_group["group_size"].toInt(),
+                          json_enemy_group["road_to_spawn"].toInt());
     }
+    enemy_groups_.push_back(std::move(groups));
   }
 }
