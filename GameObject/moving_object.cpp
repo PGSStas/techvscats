17558b0055b50a8d292a8c7b2e632d6c9f7c841c
Stanislav Katsuba <stas.katsuba@gmail.com>
2020-04-26 15:25:19 +0300

    Fix unit teleportation

--- GameObject/moving_object.cpp
+++ GameObject/moving_object.cpp
@@ -20,8 +20,10 @@ void MovingObject::MoveToDestination() {
     move_direction /= move_direction.GetLength();
     move_direction *= delta_tick_time_ * speed_ / constants::kTimeScale;
   }
-  if ((position_ + move_direction).GetVectorTo(destination_).width
-      * position_.GetVectorTo(destination_).width <= 0) {
+  auto first_vector = (position_ + move_direction).GetVectorTo(destination_);
+  auto second_vector = position_.GetVectorTo(destination_);
+  if (first_vector.width * second_vector.width <= constants::kEpsilon &&
+      first_vector.height * second_vector.height <= constants::kEpsilon) {
     position_ = destination_;
   } else {
     position_ += move_direction;
