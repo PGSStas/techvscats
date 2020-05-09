a5e92083621188bcbef2511ec3ff8b09e6660899
Maria Zaharneva <mary.virtuozo@gmail.com>
2020-05-09 17:05:08 +0300

    Travis fix

--- Model/sound_vector.cpp
+++ Model/sound_vector.cpp
@@ -12,7 +12,6 @@ SoundVector::SoundVector(const QString& path, int audio_roads_count) {
     sounds_[i]->playlist()->setCurrentIndex(0);
     playlists_.push_back(std::move(playlist));
     roads_count_++;
-    playlists_.push_back(std::move(playlist));
   }
 }
 
